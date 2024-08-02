8789b611c61a922ec7481d7e3556c282050e061d
bentleylab <113469432+bentleylab@users.noreply.github.com>
2024-08-02 09:15:59 -0500

    Update LoadInfoFile.m

--- Helper Functions/LoadInfoFile.m
+++ Helper Functions/LoadInfoFile.m
@@ -1,1 +1,5 @@
-
+function INFO = LoadInfoFile()
+% This function loads the INFO file into matlab, simply change the path to
+% where you have stored it
+    load('/Users/anaskhan/Library/CloudStorage/Box-Box/Data/PT_INFO.mat')
+end
